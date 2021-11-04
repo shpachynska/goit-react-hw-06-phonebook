a8b69b7071f42c2a1db37d3863f63ffef7b57dd1
Viktoriya Shpachynska <viktoriya.shpachynska@gmail.com>
2021-11-04 22:29:37 +0100

    step 2 with functions

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
-import React, { Component } from "react";
+import React from "react";
+import { useState } from "react";
 import ContactList from "./components/ContactList/ContactList";
 import Filter from "./components/Filter/Filter";
 import Form from "./components/Form/Form";
@@ -9,138 +10,34 @@ import IconButton from "./components/IconButton/IconButton";
 import { ReactComponent as AddIcon } from "./components/IconButton/add.svg";
 import Container from "./components/Container/Container";
 
-class App extends Component {
-  state = {
-    showModal: false,
+export default function App() {
+  const [showModal, setShowModal] = useState(false);
+
+  const toggleModal = () => {
+    setShowModal(!showModal);
   };
+  return (
+    <Container>
+      <h1 className="title">Phonebook</h1>
+      <IconButton onClick={toggleModal}>
+        <AddIcon width="40" height="40" fill="#fff" />
+      </IconButton>
 
-  toggleModal = () => {
-    this.setState((state) => ({
-      showModal: !state.showModal,
-    }));
-  };
-
-  render() {
-    const { showModal } = this.state;
-    return (
-      <Container>
-        <h1 className="title">Phonebook</h1>
-        <IconButton onClick={this.toggleModal}>
-          <AddIcon width="40" height="40" fill="#fff" />
-        </IconButton>
-
-        {showModal && (
-          <Modal onClose={this.toggleModal}>
-            <div className="form-section">
-              <div className="form">
-                <Form onSave={this.toggleModal} />
-              </div>
+      {showModal && (
+        <Modal onClose={toggleModal}>
+          <div className="form-section">
+            <div className="form">
+              <Form onSave={toggleModal} />
             </div>
-          </Modal>
-        )}
+          </div>
+        </Modal>
+      )}
 
-        <div className="contacts-section">
-          <b className="title">Contacts</b>
-          <Filter />
-          <ContactList />
-        </div>
-      </Container>
-    );
-  }
+      <div className="contacts-section">
+        <b className="title">Contacts</b>
+        <Filter />
+        <ContactList />
+      </div>
+    </Container>
+  );
 }
-export default App;
-
-// import "./App.css";
-// import React, { useEffect, useState } from "react";
-// import shortid from "shortid";
-// import ContactList from "./components/ContactList/ContactList";
-// import Filter from "./components/Filter/Filter";
-// import Form from "./components/Form/Form";
-// //================================================
-// import Modal from "./components/Modal/Modal";
-// import IconButton from "./components/IconButton/IconButton";
-// import { ReactComponent as AddIcon } from "./components/IconButton/add.svg";
-// import Container from "./components/Container/Container";
-
-// const initialContacts = [
-//   { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
-//   { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
-//   { id: "id-3", name: "Eden Clements", number: "645-17-79" },
-//   { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
-// ];
-
-// export default function App() {
-//   const [contacts, setContacts] = useState(
-//     () => JSON.parse(window.localStorage.getItem("contacts")) ?? initialContacts
-//   );
-//   const [filter, setFilter] = useState("");
-//   const [showModal, setShowModal] = useState(false);
-
-//   const addContact = (data) => {
-//     const contact = {
-//       id: shortid.generate(),
-//       name: data.name,
-//       number: data.number,
-//     };
-//     if (contacts.find((contact) => contact.name === data.name)) {
-//       alert(`${data.name} is already in contacts`);
-//       return;
-//     } else {
-//       setContacts([contact, ...contacts]);
-//     }
-//     toggleModal();
-//   };
-
-//   const changeFilter = (e) => {
-//     setFilter(e.currentTarget.value);
-//   };
-
-//   const getVisibleContacts = () => {
-//     const normalizedFilter = filter.toLowerCase();
-//     return contacts.filter((contact) =>
-//       contact.name.toLowerCase().includes(normalizedFilter)
-//     );
-//   };
-
-//   const deleteContact = (contactId) => {
-//     setContacts(() => contacts.filter((contact) => contact.id !== contactId));
-//   };
-
-//   useEffect(() => {
-//     localStorage.setItem("contacts", JSON.stringify(contacts));
-//   }, [contacts]);
-
-//   //============ modal window ============================
-//   const toggleModal = () => {
-//     setShowModal(!showModal);
-//   };
-//   //======================================================
-//   const visibleContacts = getVisibleContacts();
-//   return (
-//     <Container>
-//       <h1 className="title">Phonebook</h1>
-//       <IconButton onClick={toggleModal} aria-label="add contact">
-//         <AddIcon width="40" height="40" fill="#fff" />
-//       </IconButton>
-
-//       {showModal && (
-//         <Modal onClose={toggleModal}>
-//           <div className="form-section">
-//             <div className="form">
-//               <Form onSubmit={addContact} />
-//             </div>
-//           </div>
-//         </Modal>
-//       )}
-
-//       <div className="contacts-section">
-//         <b className="title">Contacts</b>
-//         <Filter value={filter} onChange={changeFilter} />
-//         <ContactList
-//           contacts={visibleContacts}
-//           onDeleteContact={deleteContact}
-//         />
-//       </div>
-//     </Container>
-//   );
-// }
--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,69 +1,71 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import styles from "./Form.module.css";
 import { connect } from "react-redux";
 import phonebookActions from "../../redux/phonebook/phonebook-actions";
-// import shortid from "shortid";
 
-class Form extends Component {
-  state = {
-    name: "",
-    number: "",
-  };
+function Form({ onSave, onSubmit }) {
+  const [name, setName] = useState("");
+  const [number, setNumber] = useState("");
 
-  handleChange = (event) => {
+  const handleChange = (event) => {
     const { name, value } = event.currentTarget;
-    this.setState({ [name]: value });
+    switch (name) {
+      case "name":
+        setName(value);
+        break;
+      case "number":
+        setNumber(value);
+        break;
+      default:
+        return;
+    }
   };
 
-  handleSubmit = (event) => {
+  const handleSubmit = (event) => {
     event.preventDefault();
-    this.props.onSubmit(this.state);
-    this.props.onSave();
+    onSubmit({ name, number });
+    onSave(name, number);
 
-    this.reset();
+    reset();
   };
 
-  reset = () => {
-    this.setState({
-      name: "",
-      number: "",
-    });
+  const reset = () => {
+    setName(name);
+    setNumber(number);
   };
 
-  render() {
-    return (
-      <form onSubmit={this.handleSubmit}>
-        <label>
-          <p>Name</p>
-          <input
-            type="text"
-            name="name"
-            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-            title="Имя может состоять только из букв, апострофа, тире и пробелов. Например Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan и т. п."
-            required
-            value={this.state.name}
-            onChange={this.handleChange}
-          />
-        </label>
-        <label>
-          <p>Number</p>
-          <input
-            type="tel"
-            name="number"
-            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-            title="Номер телефона должен состоять цифр и может содержать пробелы, тире, круглые скобки и может начинаться с +"
-            required
-            value={this.state.number}
-            onChange={this.handleChange}
-          />
-        </label>
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        <p>Name</p>
+        <input
+          type="text"
+          name="name"
+          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          title="Имя может состоять только из букв, апострофа, тире и пробелов. Например Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan и т. п."
+          required
+          value={name}
+          onChange={handleChange}
+        />
+      </label>
+      <label>
+        <p>Number</p>
+        <input
+          type="tel"
+          name="number"
+          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+          title="Номер телефона должен состоять цифр и может содержать пробелы, тире, круглые скобки и может начинаться с +"
+          required
+          value={number}
+          onChange={handleChange}
+        />
+      </label>
 
-        <button className={styles.button} type="submit">
-          Add contact
-        </button>
-      </form>
-    );
-  }
+      <button className={styles.button} type="submit">
+        Add contact
+      </button>
+    </form>
+  );
 }
 
 const mapDispatchToProps = (dispatch) => ({
@@ -71,71 +73,3 @@ const mapDispatchToProps = (dispatch) => ({
 });
 
 export default connect(null, mapDispatchToProps)(Form);
-// export default Form;
-
-// import React, { useState } from "react";
-// import styles from "./Form.module.css";
-
-// export default function Form({ onSubmit }) {
-//   const [name, setName] = useState("");
-//   const [number, setNumber] = useState("");
-
-//   const handleChange = (event) => {
-//     const { name, value } = event.currentTarget;
-//     switch (name) {
-//       case "name":
-//         setName(value);
-//         break;
-//       case "number":
-//         setNumber(value);
-//         break;
-//       default:
-//         return;
-//     }
-//   };
-
-//   const handleSubmit = (event) => {
-//     event.preventDefault();
-//     onSubmit({ name, number });
-
-//     reset();
-//   };
-
-//   const reset = () => {
-//     setName(name);
-//     setNumber(number);
-//   };
-
-//   return (
-//     <form onSubmit={handleSubmit}>
-//       <label>
-//         <p>Name</p>
-//         <input
-//           type="text"
-//           name="name"
-//           pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-//           title="Имя может состоять только из букв, апострофа, тире и пробелов. Например Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan и т. п."
-//           required
-//           value={name}
-//           onChange={handleChange}
-//         />
-//       </label>
-//       <label>
-//         <p>Number</p>
-//         <input
-//           type="tel"
-//           name="number"
-//           pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-//           title="Номер телефона должен состоять цифр и может содержать пробелы, тире, круглые скобки и может начинаться с +"
-//           required
-//           value={number}
-//           onChange={handleChange}
-//         />
-//       </label>
-
-//       <button className={styles.button} type="submit">
-//         Add contact
-//       </button>
-//     </form>
-//   );
-// }
